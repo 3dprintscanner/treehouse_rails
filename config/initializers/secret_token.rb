75bcd5577d1a813a1111d76df44c6c23d5ffce43
3dprintscanner <1988anthonythomas@gmail.com>
2014-11-02 21:21:24 +0000

    generated rails application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Odot::Application.config.secret_token = 'ab143efaebef09326e2dff3a3b31e1b3e11ea33b51426dad915d5baa54e11177238ce9ed8acdd82d110055188cc000f08426584c071743366e12e3dca09467aa'
