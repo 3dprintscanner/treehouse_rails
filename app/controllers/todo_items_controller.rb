247ebf376b495cad2528ef98ffade94afb4fa13e
3dprintscanner <1988anthonythomas@gmail.com>
2014-11-11 00:33:53 +0000

    Added delete destroy spec

--- app/controllers/todo_items_controller.rb
+++ app/controllers/todo_items_controller.rb
@@ -1,4 +1,6 @@
 class TodoItemsController < ApplicationController
+  
+
   def index
   	@todo_list = TodoList.find(params[:todo_list_id])
   end
@@ -40,8 +42,20 @@ class TodoItemsController < ApplicationController
       render action: :edit
     end
   end
-  # private
-  # def todo_item_params
-  # 	params[:todo_item].(:content)
-  # end
+
+  def destroy
+    @todo_list = TodoList.find(params[:todo_list_id])
+    @todo_item = @todo_list.todo_items.find(params[:id])
+    if @todo_item.destroy
+      flash[:success] ="Todo list item was deleted"
+    else
+      flash[:error] = "Todo list item could not be deleted"
+    end
+    redirect_to todo_list_todo_items_path
+  end
+  private
+  def find_todo_list
+    @todo_list = TodoList.find(params[:todo_list_id])
+    @todo_item = @todo_list.todo_items.find(params[:id]) 
+  end
 end
